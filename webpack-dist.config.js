934dbb76acd4541c0b2a29874ab0b9850c56f390
Galaxy-DH <414318370@qq.com>
2021-05-26 22:44:45 +0800

    test

--- webpack-dist.config.js
+++ webpack-dist.config.js
@@ -54,12 +54,14 @@ module.exports = {
     new OptimizeCSSAssetsPlugin(),
     new EndWebpackPlugin(async () => {
       // 自定义域名
-      fs.writeFileSync(path.resolve(outputPath, 'CNAME'), 'resume.wuhaolin.cn');
+      //fs.writeFileSync(path.resolve(outputPath, 'CNAME'), 'resume.wuhaolin.cn');
+      fs.writeFileSync(path.resolve(outputPath, 'CNAME'), 'galaxy-dh.github.io');
 
       // 调用 Chrome 渲染出 PDF 文件
       const chromePath = findChrome();
       spawnSync(chromePath, ['--headless', '--disable-gpu', `--print-to-pdf=${path.resolve(outputPath, 'resume.pdf')}`,
-        'http://resume.wuhaolin.cn' // 这里注意改成你的在线简历的网站
+        'https://galaxy-dh.github.io' // 这里注意改成你的在线简历的网站
+        //'http://resume.wuhaolin.cn' // 这里注意改成你的在线简历的网站
       ]);
     }),
   ]
